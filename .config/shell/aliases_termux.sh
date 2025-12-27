0a8f0255b42dffb440b51f2fb05c7dfe548ae02c
KYBNgh <kybnol@outlook.com>
2025-12-27 11:55:45 +0800

    copy aliases from .config/shell/aliases.sh to .cnfig/shell/aliases_termux.sh

--- .config/shell/aliases_termux.sh
+++ .config/shell/aliases_termux.sh
@@ -1,2 +1,30 @@
+alias l='ls --color=auto'
+alias ls='ls -F --color=auto --group-directories-first'
+alias ll="ls -lh --time-style=long-iso"
+alias la="ls -A"
+alias lla="ls -lhA --time-style=long-iso"
+alias l.="ls -d .*"
+alias ll.="ls -lh -d --time-style=long-iso .*"
+alias lt="ls -lt --time-style=long-iso"
+alias lw="ls -ldh $(pwd)"
+alias lss='du -sh .[^.]* .[!.]* * 2>/dev/null | sort -hr'
+
+alias grep='grep --color=auto'
+
+alias md='mkdir'
+alias rd='rmdir'
+alias rm='rm -I'
+alias cp='cp -i'
+alias mv='mv -i'
+
+alias virc='vim ~/.bashrc'
+alias vipf='vim ~/.profile'
+alias vialias='vim ~/.config/shell/aliases_termux.sh'
+alias srpf='. ~/.profile'
+alias srrc='. ~/.bashrc'
+
+alias c='clear'
+alias vim='nvim'
+
 alias sshpc='ssh -p 7891 qwq@192.168.1.3'
 alias sync-doc='rsync -av --exclude="containers" arch:/home/qwq/doc/ ~/doc'
