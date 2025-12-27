dc3ecc1ff675ccae3bdf57b4beee3153b57958a2
KYBNgh <kybnol@outlook.com>
2025-12-27 11:30:38 +0800

    fix a wrong alias: sync-doc

--- .config/shell/aliases_termux.sh
+++ .config/shell/aliases_termux.sh
@@ -1,2 +1,2 @@
 alias sshpc='ssh -p 7891 qwq@192.168.1.3'
-alias sync-doc='rsync -av --exclude="containers" =arch:/home/qwq/doc/ ~/doc'
+alias sync-doc='rsync -av --exclude="containers" arch:/home/qwq/doc/ ~/doc'
