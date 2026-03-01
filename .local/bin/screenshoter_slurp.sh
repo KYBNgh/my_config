4f37e86c402ac4b0ed5d84216960a8dbbd0543c2
KYBNgh <kybnol@outlook.com>
2026-03-01 15:49:43 +0800

    remove ~/.local/bin/debian-12 : out of date improve screenshoter: copy to clipboard improve comment of waybar-net-mib.sh

--- .local/bin/screenshoter_slurp.sh
+++ .local/bin/screenshoter_slurp.sh
@@ -2,6 +2,9 @@
 filename=$(date +%Y%m%d-%H%M%S).png
 filedir=${HOME}/pic/shot
 if grim -g "$(slurp)" "${filedir}/${filename}"; then
-    notify-send -u low -i camera-photo-symbolic 'Screenshot' "Saved: ${filename}"
+    wl-copy < "$filedir/$filename"
+    notify-send -u low -i camera-photo-symbolic 'Screenshot' "Saved: ${filename} and copied to clipboard"
 fi
 
+# This is fallback option
+# grim -g "$(slurp)" && notify-send 'Failed to save to ~/pic/shot/' 'running fall back command...'
