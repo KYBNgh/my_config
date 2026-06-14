e5df16c8cc51d5cb833b522f394dcac55858f307
KYBNgh <kybnol@outlook.com>
2026-06-14 10:18:49 +0800

    modified:   .config/bash/multi/completions/proxyctl modified:   .config/bash/multi/proxy.sh

--- .config/bash/multi/proxy.sh
+++ .config/bash/multi/proxy.sh
@@ -1,6 +1,6 @@
 # ~/.config/bash/multi/proxy.sh
 
-proxy_drop() {
+_proxy_drop() {
     unset http_proxy https_proxy ftp_proxy no_proxy
     unset HTTP_PROXY HTTPS_PROXY FTP_PROXY NO_PROXY
     echo "All proxy setting jumped to drop"
@@ -10,7 +10,7 @@ proxyctl() {
     case "$1" in
         lan)
             case "$2" in
-                up|on|start)
+                up)
                     export http_proxy="http://192.168.31.20:8080"
                     export https_proxy="$http_proxy"
                     export ftp_proxy="$http_proxy"
@@ -20,10 +20,10 @@ proxyctl() {
                     export HTTPS_PROXY="$https_proxy"
                     export FTP_PROXY="$ftp_proxy"
                     export NO_PROXY="$no_proxy"
-                    echo "Lan proxy $http_proxy is up"
+                    echo "Lan proxy $http_proxy is set to up"
                     ;;
-                down|off|stop|drop)
-                    proxy_drop
+                down|drop)
+                    _proxy_drop
                     ;;
                 *)
                     echo "Usage: proxyctl lan {up|down}"
@@ -31,9 +31,9 @@ proxyctl() {
                     ;;
             esac
             ;;
-        lo|local)
+        lo)
             case "$2" in
-                up|on|start)
+                up)
                     export http_proxy="http://127.0.0.1:7897"
                     export https_proxy="$http_proxy"
                     export ftp_proxy="$http_proxy"
@@ -43,10 +43,10 @@ proxyctl() {
                     export HTTPS_PROXY="$https_proxy"
                     export FTP_PROXY="$ftp_proxy"
                     export NO_PROXY="$no_proxy"
-                    echo "Local proxy $http_proxy is up"
+                    echo "Local proxy $http_proxy is set to up"
                     ;;
-                down|off|stop|drop)
-                    proxy_drop
+                down|drop)
+                    _proxy_drop
                     ;;
                 *)
                     echo "Usage: proxyctl lo {up|down}"
@@ -54,8 +54,8 @@ proxyctl() {
                     ;;
             esac
             ;;
-        drop|off|down)
-            proxy_drop
+        drop|down)
+            _proxy_drop
             ;;
         status)
             echo "http_proxy  = ${http_proxy:-Not set yet}"
